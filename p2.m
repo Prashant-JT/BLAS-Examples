2c4b94c66ea512f2703ab18b9b5f3751024d2c5c
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2020-11-09 19:47:55 +0000

    have to complete exercise 3

---
+++ p2.m
@@ -0,0 +1,25 @@
+% Ejercicio 1
+v1 = [3.0, -1.0, -3.0];
+v2 = [4.0, 3.0, 3.0];
+
+res1 = dot(v1, v2);
+
+% Ejercicio 2
+name = 'jose maria';
+vN = double(name);
+surname = 'jeswani000';
+vA = double(surname);
+vA = 3 * vA;
+
+res2 = vN + vA;
+
+
+% Ejercicio 3
+d = [24, 5, 1999];
+
+res3 = mod (norm(d, 2), 11);
+
+% Ejercicio 4 (optativo)
+% ir modificado los parametros 
+% del ejercicio anterior
+
