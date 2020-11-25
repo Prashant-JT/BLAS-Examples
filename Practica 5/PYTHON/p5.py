3f26c65a31ef3c5c62677d6ebde43bc3ccec2886
JoseMAP-99 <55493484+JoseMAP-99@users.noreply.github.com>
2020-11-25 18:03:51 +0000

    Add files via upload

--- Practica 5/PYTHON/p5.py
+++ Practica 5/PYTHON/p5.py
@@ -1,22 +1,24 @@
 import time
 import numpy as np
+import csv
+
 
 def function1():
-    for i in range(2, 501):
-        A = np.random.rand(i,i)
-        B = np.random.rand(i,i)
+    total_time = []
+    for i in range(100, 1001, 20):
+        A = np.random.rand(i, i)
+        B = np.random.rand(i, i)
 
         start = time.time()
         for j in range(0, 100):
-            np.dot(A,B)
-        fin= time.time()
-        total_time = (fin - start) / 100
-        
-        print(total_time)
+            np.dot(A, B)
+        fin = time.time()
+        total_time.append((fin - start) / 100)
+
+    with open("Ejercicion5PYTHON.csv", 'w', newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(total_time)
 
 
 if __name__ == "__main__":
-        function1()
-        #A = np.random.rand(2,2)
-        #A.astype(double)
-        #print(A.dtype)
+    function1()
